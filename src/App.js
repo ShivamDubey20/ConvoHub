e26d87a3594b1b297924a34e3a2cdf622e3492fd
shivamkd20@gmail.com <Shivam Dubey>
2023-05-19 20:35:57 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import { ChatEngine } from 'react-chat-engine';
+
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
+import './App.css';
+
+const projectID = '364a232f-7fd3-4fa1-9be9-4e90df5bd5b4';
+
+const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+    />
+  );
+};
+
+
+export default App;
